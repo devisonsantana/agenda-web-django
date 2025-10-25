12723b82536a43fa0abd9fa84986e82c8fd20944
devisonsantana <devisonsantana@hotmail.com>
2025-10-25 02:41:29 -0300

    feat: create event model and register into database

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from events.models import Event
 
 # Register your models here.
+
+class AdminEvent(admin.ModelAdmin):
+    list_display = ('title', 'description', 'event_date', 'created_at', 'user')
+    list_filter = ('title', 'event_date', 'created_at')
+
+admin.site.register(Event, AdminEvent)
