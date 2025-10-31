5b3f557d5fc522b8fb4254a5cca1b37bd23bbb74
devisonsantana <devisonsantana@hotmail.com>
2025-10-30 21:14:37 -0300

    chore(deploy): modify builder vercel

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+
+echo "Upgrading pip"
+
+python3 -m pip install --upgrade pip
+
+echo "Installing requirements"
+
 python3 -m pip install -r requirements.txt
+
+echo "Creating static files"
+
 python3 manage.py collectstatic --noinput
