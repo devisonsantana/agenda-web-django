4d583d8461d6d8e4c708bd3a48c2b926af0bb54a
devisonsantana <devisonsantana@hotmail.com>
2025-10-30 21:30:07 -0300

    feat(deploy): add static files storage

--- agenda/settings.py
+++ agenda/settings.py
@@ -121,6 +121,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
