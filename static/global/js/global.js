394e8e712cdc3884e210c949ffe1df6f464398df
devisonsantana <devisonsantana@hotmail.com>
2025-11-06 14:44:58 -0300

    feat: implement global files
    - remove duplicated and unused files
    - add static dir global

--- static/global/js/global.js
+++ static/global/js/global.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body;
     const toggleBtn = document.getElementById('theme-toggle');
     const themeIcon = document.getElementById('theme-icon');
 
-    if (localStorage.getItem('theme') === 'dark-mode') {
-        document.body.classList.add('dark-mode');
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme === 'dark') {
+        body.classList.add('dark-mode');
         themeIcon.className = 'bi bi-sun';
     }
 
-    toggleBtn.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        const isDark = document.body.classList.contains('dark-mode');
+    toggleBtn?.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        const isDark = body.classList.contains('dark-mode');
         themeIcon.className = isDark ? 'bi bi-sun' : 'bi bi-moon';
         localStorage.setItem('theme', isDark ? 'dark' : 'light');
     });
