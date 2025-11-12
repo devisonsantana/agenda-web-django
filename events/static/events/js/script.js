e8256affedb7c8869f83f10a838c11963a541de3
devisonsantana <devisonsantana@hotmail.com>
2025-11-11 23:54:50 -0300

    chore: remove script to delete a event

--- events/static/events/js/script.js
+++
@@ -1,5 +0,0 @@
-function confirmDelete(eventId, eventTitle) {
-    if (confirm(`Deseja realmente excluir o evento "${eventTitle}"?`)) {
-        window.location.href = `/agenda/delete/${eventId}`;
-    }
-}
