e44f49d0e55360828270a78cb50572d8bfd6f00c
devisonsantana <devisonsantana@hotmail.com>
2025-11-14 02:37:42 -0300

    chore: remove unfunctional link and set users profiles new path

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+def upload_to(instace, filename):
+    return f'profile_pics/{instace.username}/{filename}'
+
 class CustomUser(AbstractUser):
     profile_picture = models.ImageField(
-        upload_to='profile_pics',
+        upload_to=upload_to,
         default='profile_pics/default-profile.jpg',
         blank=True,
         null=True,
